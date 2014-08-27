727b32e79e18d055927bb9045e632eebaadbb470
Harry Ng <harryworld@gmail.com>
2014-08-27 10:07:51 +0800

    Add bootstrap

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -1,5 +1,6 @@
 class DevelopersController < ApplicationController
   before_action :set_developer, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /developers
   # GET /developers.json
